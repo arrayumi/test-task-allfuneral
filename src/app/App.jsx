778e658e53a7b5f1c68e3043237bc1e010469108
Anna <thoria@yandex.ru>
2025-04-09 06:03:29 +0300

    add navigate to basepath be first render

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,9 +10,11 @@ import * as page from "../pages";
 
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { isAuthorized } = useSelector(getUserData);
 
   useEffect(() => {
+    navigate("/");
     if (!isAuthorized) {
       dispatch(authorize());
     } else {
@@ -24,7 +26,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<page.Profile />}>
-        <Route path="/organizations/12" element={<page.Organizations />}></Route>
+        <Route
+          path="/organizations/12"
+          element={<page.Organizations />}
+        ></Route>
         <Route path="/contractors" element={<page.Contractors />}></Route>
         <Route path="/clients" element={<page.Clients />}></Route>
       </Route>
